2e5d9c41119c31660bdea225da996c2dfa65509f
Ubuntu <ubuntu@ip-172-31-1-211.ap-southeast-2.compute.internal>
2015-06-28 08:03:46 +0000

    Added header.php and footer.php files that have the start of the html and header/nav; and have the footer and end of the HTML, for reusability
    Added search.php for part 1 of assignment. So far this is just the basic layout of HTML with no database
    queryies as of yet.
    
    Modified index.php to use new header and footer php files.

---
+++ php/footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>test.php</title>
+</head>
+
+<body>
+<h1>Test output for database</h1>
+<br>
+<table>
+   <tr>
+      <th>Customer ID</th>
+      <th>Surname</th>
+      <th>Firstname</th>
+      <th>Initial</th>
+      <th>Title ID</th>
+      <th>Address</th>
+   </tr>
+   
+   <?php
+      # use connect.php to connect to database
+      require_once('php/connect.php');
+
+      # generate query and query database connection
+      $query = "SELECT * FROM customer";
+      $result = mysql_query($query);
+
+      # check if query returned is blank and display error
+      if (!$result) {
+         $message = 'Invalid query: ' . mysql_error() . "\n";
+         $message .= 'Whole query: ' . $query;
+         die($message);
+      }
+      else {
+         echo '<p>data returned</p>';
+      }
+
+      # output result
+      while ($row = mysql_fetch_assoc($result)) {
+         echo '<tr>';
+         echo '<td>' . $row['cust_id'] . '</td>';
+         echo '<td>' . $row['surname'] . '</td>';
+         echo '<td>' . $row['firstname'] . '</td>';
+         echo '<td>' . $row['initial'] . '</td>';
+         echo '<td>' . $row['title_id'] . '</td>';
+         echo '<td>' . $row['address'] . '</td>';
+         echo '</tr>';
+      }
+
+      # free resources associated with the result set
+      # close database connection
+      mysql_free_result($result);
+      mysql_close($dbconn);
+   ?>
+   </table>
+</body>
+</html>
