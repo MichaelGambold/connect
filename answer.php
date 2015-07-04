e30c754be351d91a8fafcc1c93fb1bca9363c018
Ubuntu <ubuntu@ip-172-31-1-211.ap-southeast-2.compute.internal>
2015-07-04 09:04:41 +0000

    Updated test output ot write to variables for testing. Removed echo for $_GET and replaced with variables.
    Created array $where to have the WHERE clause required for database query. This is being
    imploaded with ' AND '.
    
    Updated SQL statment with forgotten table.
    
    Created session to test passing results to next page.
    
    Bind optional variables to PDO statement.
    
    Competed getting min and max years from database and adding as min and max for number input fields.
    
    Fixed other various bugs.

--- answer.php
+++ answer.php
@@ -1,27 +1,40 @@
 <?php
+   // start session
+   session_start();
+
+   // add required files
    require 'php/connect.php';
+   
+   // assign get paramaters to variables
+   $wineName = $_GET['wineName'];
+   $wineryName = $_GET['wineryName'];
+   $region = $_GET['region'];
+   $grapeVariety = $_GET['grapeVariety'];
+   $minYear = $_GET['minYear'];
+   $maxYear = $_GET['maxYear'];
+   $minWinesInStock = $_GET['minWinesInStock'];
+   $minWinesOrdered = $_GET['minWinesOrdered'];
+   $minCost = $_GET['minCost'];
+   $maxCost = $_GET['maxCost'];   
 
    // display variables
-   echo 'wine name: ' . $_GET['wineName'] . '<br>';
-   echo 'winery name: ' . $_GET['wineryName'] . '<br>';
-   echo 'region: ' . $_GET['region'] . '<br>';
-   echo 'grape variety: ' . $_GET['grapeVariety'] . '<br>';
-   //echo 'min year: ' . $_GET['minYear'] . '<br>';
-   //echo 'max year: ' . $_GET['maxYear'] . '<br>';
-   echo 'min wine in stock: ' . $_GET['minWineInStock'] . '<br>';
-   echo 'min wines ordered: ' . $_GET['minWinesOrdered'] . '<br>';
-   echo 'min cost: ' . $_GET['minCost'] . '<br>';
-   echo 'max cost: ' . $_GET['maxCost'] . '<br>';
-
-   // connect to database
-   $db = db_connect();
+   echo 'wine name: ' . $wineName . '<br>';
+   echo 'winery name: ' . $wineryName . '<br>';
+   echo 'region: ' . $region . '<br>';
+   echo 'grape variety: ' . $grapeVariety . '<br>';
+   echo 'min year: ' . $minYear . '<br>';
+   echo 'max year: ' . $maxYear . '<br>';
+   echo 'min wine in stock: ' . $minWinesInStock . '<br>';
+   echo 'min wines ordered: ' . $minWinesOrdered . '<br>';
+   echo 'min cost: ' . $minCost . '<br>';
+   echo 'max cost: ' . $maxCost . '<br>';
 
    // perform basic server side validation
    try {
       // check years not less or greater than years in database
    
       // check min wine in stock value is not negative
-      if (!empty($_GET['minWineInStock']) && $_GET['minWinInStock'] < 0) {
+      if (!empty($_GET['minWinesInStock']) && $_GET['minWinesInStock'] < 0) {
          throw new Exception('Min wine in stock cannot be negative');
       }
 
@@ -45,14 +58,45 @@
          throw new Exception('Max Cost cannot be negative');
       }
    } catch (Exception $e) {
-      // close database connection
-      $db = null;
+      // kill page with error
+      die('Error: Invalid search parameters');
    }
+
+   // connect to database
+   $db = db_connect();
+   
+   // create array of where statments
+   $where = array();
+   $where[] = "w.year >= :minYear";
+   $where[] = "w.year <=:maxYear";
+   $where[] = "inv.on_hand >= :numInStock";
+   $where[] = "ord.ordered >= :numOrdered";
+
+   if (!empty($_GET['wineName']))
+      $where[] = "w.wine_name LIKE CONCAT('%', :wineName, '%')";
+   
+   if (!empty($_GET['wineryName']))
+      $where[] = "wy.winery_name LIKE CONCAT('%', :wineryName, '%')";
+
+   if (!empty($_GET['region']) && $_GET['region'] != 'All')
+      $where[] = "r.region LIKE CONCAT('%', :region, '%')";
+
+   if (!empty($_GET['grapeVariety']) && $_GET['grapeVariety'] != 'All')
+      $where[] = "gv.grape_blend LIKE CONCAT('%', :grapeVariety, '%')";
+
+   if (!empty($_GET['minCost']))
+      $where[] = "inv.cost >= :minCost";
+
+   if (!empty($_GET['maxCost']))
+      $where[] = "inv.cost <= :maxCost";
+
+//echo 'where statement: ' . implode(' AND ', $where);
+   
 
    // create database statment based on inputs available
    echo '<br>testing for matches to wine name<br><br>';
    
-   $stmt = $db->prepare("SELECT w.wine_name, wt.wine_type, w.year, wy.winery_name, r.region_name, gv.grape_blend, inv.on_hand, ord.ordered 
+   $stmt = $db->prepare("SELECT w.wine_name, wt.wine_type, w.year, wy.winery_name, r.region_name, gv.grape_blend, inv.cost, inv.on_hand, ord.ordered 
                          FROM wine w 
                          JOIN wine_type wt ON wt.wine_type_id = w.wine_type 
                          JOIN winery wy ON wy.winery_id = w.winery_id 
@@ -65,14 +109,39 @@
                          JOIN inventory inv ON inv.wine_id = w.wine_id 
                          JOIN (SELECT wine_id, SUM(qty) AS ordered 
                                FROM items 
-                               GROUP BY wine_id) AS ord ON ord.wine_id = w.wine_id;");
-      //$stmt = $db->prepare("SELECT w.wine_name, wt.wine_type, w.year, wy.winery_name
-                           // FROM wine w");
-                           // JOIN wine_type wt
-                           // ON wt.wine_type = w.wine_type
-                           // JOIN winery wy
-                           // ON wy.winery_id = w.winery_id");
-                           // WHERE w.wine_name LIKE '%" . $_GET['wineName'] . "%'");
+                               GROUP BY wine_id) AS ord ON ord.wine_id = w.wine_id
+                         WHERE " . implode(' AND ', $where));
+   
+   // add '%' wild cards for sql parameters where required for LIKE WHERE clauses
+   /*$wineName = '%' . $wineName . '%';
+   $wineryName = '%' . $wineryName . '%';
+   $region = '%' . $region . '%';
+   $grapeVariety = '%' . $grapeVarity . '%';*/
+
+   // bind standard parameters
+   $stmt->bindParam(':minYear', $_GET['minYear'], PDO::PARAM_INT);
+   $stmt->bindParam(':maxYear', $_GET['maxYear'], PDO::PARAM_INT);
+   $stmt->bindParam(':numInStock', $_GET['minWinesInStock'], PDO::PARAM_INT);
+   $stmt->bindParam(':numOrdered', $_GET['minWinesOrdered'], PDO::PARAM_INT);
+
+   // bind optional parameters if required
+   if (!empty($_GET['wineName']))
+      $stmt->bindParam(':wineName', $_GET['wineName'], PDO::PARAM_STR);
+
+   if (!empty($_GET['wineryName']))
+      $stmt->bindParam(':wineryName', $wineryName, PDO::PARAM_STR);
+   
+   if (!empty($_GET['region']) && $_GET['region'] != 'All')
+      $stmt->bindParam(':region', $region, PDO::PARAM_STR);
+
+   if (!empty($_GET['grapeVariety']) && $_GET['grapeVariety'] != 'All')
+      $stmt->bindParam(':grapeVariety', $_GET['grapeVariety'], PDO::PARAM_STR);
+   
+   if (!empty($_GET['minCost']))
+      $stmt->bindParam(':minCost', $_GET['minCost'], PDO::PARAM_STR);
+
+   if (!empty($_GET['maxCost']))
+      $stmt->bindParam(':maxCost', $_GET['maxCost'], PDO::PARAM_STR);
    
    $stmt->execute();
 
@@ -83,6 +152,7 @@
    echo '<th>winery_name</th>';
    echo '<th>region</th>';
    echo '<th>grape variety</th>';
+   echo '<th>cost</th>';
    echo '<th>num in stock</th>';
    echo '<th>orders</th>';
    echo '</tr>';
@@ -94,14 +164,19 @@
       echo '<td>' . $row['winery_name'] . '</td>';
       echo '<td>' . $row['region_name'] . '</td>';
       echo '<td>' . $row['grape_blend'] . '</td>';
+      echo '<td>' . $row['cost'] . '</td>';
       echo '<td>' . $row['on_hand'] . '</td>';
-      echo '<td>' . $row['ordered'] . '</td>';
-      
-
+      echo '<td>' . $row['ordered'] . '</td>'; 
       echo '</tr>';
    }
-   //$stmt = $db->prepare('');
+   
+   // save results to session variable
+   // $_SESSION['results'] = $stmt->fetchAll();
 
    // close database connection
     $db = null;
+
+   // redirect to results page
+   $resultsPage = 'results.php';
+   //header("Location: $resultsPage");
 ?>
--- search.php
+++ search.php
@@ -19,10 +19,10 @@
       <option value="">Please select</option>
       <?php
          // get regions from database connection
-         $stmt = $db->prepare("SELECT region_id, region_name FROM region");
+         $stmt = $db->prepare('SELECT region_name FROM region');
          if ($stmt->execute()) {
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-               echo '<option value="' . $row['region_id'] . '">' . $row['region_name'] . '</option>';
+               echo '<option value="' . $row['region_name'] . '">' . $row['region_name'] . '</option>';
             }
          }
       ?>
@@ -31,19 +31,32 @@
    <label for="grapeVariety">Grape Variety:</label>
    <select id="grapeVariety" name="grapeVariety">
       <option value="">Please Select</option>
+      <option value="All">All</option>
       <?php
          // get grape varieties from database connection
-         $stmt = $db->prepare("SELECT variety_id, variety FROM grape_variety");
+         $stmt = $db->prepare('SELECT variety FROM grape_variety');
          if ($stmt->execute()) {
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-               echo '<option value="' . $row['variety_id'] . '">' . $row['variety'] . '</option>';
+               echo '<option value="' . $row['variety'] . '">' . $row['variety'] . '</option>';
             }
          }
       ?>
    </select><br>
 
    <label for="years">Years:</label>
-   <?php echo 'TODO: YEAR RANGE, inc ID & NAME'; ?><br>
+   <?php 
+      $stmt = $db->prepare('SELECT MIN(year) AS minYear, MAX(year) AS maxYear FROM wine');
+      if ($stmt->execute()) {
+         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+         $minYear = $row['minYear'];
+         $maxYear = $row['maxYear'];
+
+         echo '<label for="minYear">Min Year: </label>';
+         echo '<input type="number" id ="minYear" name="minYear" value="' . $minYear . '" min="' . $minYear . '" max="' . $maxYear . '">';
+         echo '<label for="maxYear">Max Year: </label>';
+         echo '<input type="number" id ="maxYear" name="maxYear" value="' . $maxYear . '" min="' . $minYear . '" max="' . $maxYear . '">';
+      }
+   ?><br>
 
    <label for="minWinesInStock">Minimum No. Wines In Stock(per wine):</label>
    <input type="number" id="minWinesInStock" name="minWinesInStock" value="0" min="0"><br>
